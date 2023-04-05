034b260734e17fc1e829ecac230457bfa70fda21
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-04-05 20:48:50 +0200

    Managed to get full metadata result to work. Hurraygit add --all

--- ApplicationCore/analysis/news_classifier.py
+++ ApplicationCore/analysis/news_classifier.py
@@ -5,6 +5,8 @@ from analytical_classes.emo_breakdown_result import EmoBreakdownResult
 from Utils.text_divider import text_divider
 from analysis.analytical_utils.get_emo_breakdown_percentage import get_emo_breakdown_percentage
 from analysis.analytical_utils.get_emo_breakdown_from_tranches import get_emo_breakdown_from_tranches
+from analytical_classes.emo_breakdown_result_metadata import EmoBreakdownResultMetadata
+from analysis.analytical_utils.update_emo_breakdown_average import update_emo_breakdown_average
 
 
 class NewsClassifier:
@@ -58,6 +60,11 @@ class NewsClassifier:
 
                 emo_breakdown_percentage, most_emo_dict = get_emo_breakdown_percentage(emo_breakdown, result_counter, most_emo_dict)
 
+                if result_counter == 0:
+                    emo_breakdown_average = emo_breakdown_percentage
+                else:
+                    emo_breakdown_average = update_emo_breakdown_average(emo_breakdown_percentage, emo_breakdown_average, result_counter)
+
                 emo_breakdown_result = EmoBreakdownResult(
                     article.title, result['description'], result['publisher']['title'], result['published date'], article.canonical_link, emo_breakdown_percentage)
                 emo_breakdown_results.append(emo_breakdown_result)
@@ -70,4 +77,8 @@ class NewsClassifier:
 
                 result_counter += 1
 
-        return emo_breakdown_results
+        emo_breakdown_result_metadata = EmoBreakdownResultMetadata(emo_breakdown_results, nb_articles_skipped, emo_breakdown_average, emo_breakdown_results[most_emo_dict['anger']['index']], 
+                 emo_breakdown_results[most_emo_dict['disgust']['index']], emo_breakdown_results[most_emo_dict['sadness']['index']], emo_breakdown_results[most_emo_dict['joy']['index']], 
+                 emo_breakdown_results[most_emo_dict['fear']['index']], emo_breakdown_results[most_emo_dict['surprise']['index']], emo_breakdown_results[most_emo_dict['neutral']['index']])
+        
+        return emo_breakdown_result_metadata
