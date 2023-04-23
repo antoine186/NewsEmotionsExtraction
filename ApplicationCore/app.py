c92bd698b0ff88ec7d5fe770aa4c384ba460afc4
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-04-24 01:41:35 +0200

    Managed to create the incomplete subscription

--- ApplicationCore/app.py
+++ ApplicationCore/app.py
@@ -9,6 +9,7 @@ from authentication.session_authentication_blueprint import session_authenticati
 from search.emo_search_blueprint import emo_search_blueprint
 from account_data.basic_account_creation_blueprint import basic_account_creation_blueprint
 from payment.stripe_customer_creation_blueprint import stripe_customer_creation_blueprint
+from payment.subscription_creation_blueprint import subscription_creation_blueprint
 
 # app.register_blueprint(user_bp, url_prefix='/users')
 app.register_blueprint(main_page_blueprint)
@@ -17,6 +18,7 @@ app.register_blueprint(session_authentication_blueprint)
 app.register_blueprint(emo_search_blueprint)
 app.register_blueprint(basic_account_creation_blueprint)
 app.register_blueprint(stripe_customer_creation_blueprint)
+app.register_blueprint(subscription_creation_blueprint)
 
 with app.app_context():
     db.init_app(app)
--- ApplicationCore/routes/payment/subscription_creation_blueprint.py
+++ ApplicationCore/routes/payment/subscription_creation_blueprint.py
@@ -1,29 +1,54 @@
 # Set your secret key. Remember to switch to your live secret key in production.
 # See your keys here: https://dashboard.stripe.com/apikeys
 import stripe
-stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+from ApplicationCore.config import stripe_api_key
+stripe.api_key = stripe_api_key
 
 from flask import Blueprint, request, make_response
 import json
 from app_start_helper import db
 from sqlalchemy import text
 
-basic_account_creation_blueprint = Blueprint('basic_account_creation_blueprint', __name__)
+subscription_creation_blueprint = Blueprint('subscription_creation_blueprint', __name__)
 
-@basic_account_creation_blueprint.route('/api/basic-account-create', methods=['POST'])
-def basic_account_create():
+@subscription_creation_blueprint.route('/api/subscription_create', methods=['POST'])
+def subscription_create():
     payload = request.data
     payload = json.loads(payload)
 
-    stripe.Customer.create(
-    email="{{CUSTOMER_EMAIL}}",
-    name="{{CUSTOMER_NAME}}",
-    address={
-        "city": "Brothers",
-        "country": "US",
-        "line1": "27 Fredrick Ave",
-        "line2": "",
-        "postal_code": "97712",
-        "state": "CA",
-    },
-    )
+    customer_id = payload['stripeCustomerId']
+    price_id = payload['priceId']
+
+    try:
+        # Create the subscription. Note we're expanding the Subscription's
+        # latest invoice and that invoice's payment_intent
+        # so we can pass it to the front end to confirm the payment
+        subscription = stripe.Subscription.create(
+            customer=customer_id,
+            items=[{
+                'price': price_id,
+            }],
+            payment_behavior='default_incomplete',
+            payment_settings={'save_default_payment_method': 'on_subscription'},
+            expand=['latest_invoice.payment_intent'],
+        )
+
+        operation_response = {
+            "operation_success": True,
+            "responsePayload": {
+                "subscription_id": subscription.id,
+                "client_secret": subscription.latest_invoice.payment_intent.client_secret
+            },
+            "error_message": "" 
+        }
+        response = make_response(json.dumps(operation_response))
+        return response
+
+    except Exception as e:
+        operation_response = {
+            "operation_success": False,
+            "error_message": "" 
+        }
+        response = make_response(json.dumps(operation_response))
+        return response
+    
