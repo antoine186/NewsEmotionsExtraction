feaa99047e2ebf1ddb6d6d3ac563792d3a0e0ce4
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-11 01:52:52 +0200

    Testing ci-cd runner 50 Im stupid

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export FLASK_APP='ApplicationCore.app'
+
 sudo systemctl daemon-reload
 sudo systemctl start flaskapp
 sudo systemctl enable flaskapp
@@ -8,5 +10,4 @@ sudo systemctl restart nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
-export FLASK_APP='ApplicationCore.app'
 flask run --host=0.0.0.0
