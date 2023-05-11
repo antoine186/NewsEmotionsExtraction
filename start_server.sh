3aaed3ce9afb2daa98af7241553e0251ff214cd5
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-11 06:03:31 +0200

    Attempt gunicorn

--- start_server.sh
+++ start_server.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo tmux kill-server
-
 sudo systemctl daemon-reload
 sudo systemctl start flaskapp
 sudo systemctl enable flaskapp
@@ -13,9 +11,5 @@ sudo systemctl enable nginx
 cd /home/ubuntu/NewsEmotionsExtraction
 sudo python3 -m venv venv
 
-tmux new -s flask_backend_session
-
 export FLASK_APP='ApplicationCore.app'
-flask run --host=0.0.0.0
-
-exit N
+gunicorn -b 0.0.0.0:5000 ApplicationCore.app:app
