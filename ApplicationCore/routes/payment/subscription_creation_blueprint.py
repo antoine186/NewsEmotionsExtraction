b560e85c594f7dc1cb860f74071ae8a45c21ad8d
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-12 01:36:36 +0200

    Sub creation debug

--- ApplicationCore/routes/payment/subscription_creation_blueprint.py
+++ ApplicationCore/routes/payment/subscription_creation_blueprint.py
@@ -61,7 +61,7 @@ def subscription_create():
             "operation_success": False,
             "responsePayload": {
             },
-            "error_message": "" 
+            "error_message": e 
         }
         response = make_response(json.dumps(operation_response))
         return response
