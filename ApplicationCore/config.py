c1822e169014708fe88c3815edc9124a05af1fb1
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-11 04:22:06 +0200

    Attempting first production deployment ever

--- ApplicationCore/config.py
+++ ApplicationCore/config.py
@@ -9,12 +9,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-stripe_api_key = 'sk_test_51MyG0LFAAs2DFWSVpgT2ghJhCoQnlrO1Y3F29CHsGJkpyaZ8Qo5b7V2hRn8cLmqj4pWmYAI0eLKGWBZubjDsn8cw00or9QmyMd'
+#stripe_api_key = 'sk_test_51MyG0LFAAs2DFWSVpgT2ghJhCoQnlrO1Y3F29CHsGJkpyaZ8Qo5b7V2hRn8cLmqj4pWmYAI0eLKGWBZubjDsn8cw00or9QmyMd'
+stripe_api_key = 'pk_test_51MyG0LFAAs2DFWSVSYjOg0bO20EGAVi3LDtGBypqn3ScKzcn9fV9FpC0QmMHWNw9L831L3R1X7xo4Xf6K2yplsh600khIw33yT'
 
 # Connect to the database
 # postgres://{user}:{password}@{hostname}:{port}/{database-name}
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:login123@localhost:5432/postgres'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Emocritical186@database-1.ccigqpo72mbx.us-east-2.rds.amazonaws.com:5432/postgres'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:login123@localhost:5432/postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Emocritical186@database-3.ccigqpo72mbx.us-east-2.rds.amazonaws.com:5432/postgres'
 
 # Turn off the Flask-SQLAlchemy event system and warning
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo tmux kill-server
+
 sudo systemctl daemon-reload
 sudo systemctl start flaskapp
 sudo systemctl enable flaskapp
@@ -11,5 +13,9 @@ sudo systemctl enable nginx
 cd /home/ubuntu/NewsEmotionsExtraction
 sudo python3 -m venv venv
 
+tmux new -s flask_backend_session
+
 export FLASK_APP='ApplicationCore.app'
 flask run --host=0.0.0.0
+
+exit N
