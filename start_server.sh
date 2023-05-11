4d097d33369c22c4d3a9ec0994663872decc566f
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-11 06:15:38 +0200

    Attempt gunicorn 2

--- start_server.sh
+++ start_server.sh
@@ -7,9 +7,3 @@ sudo systemctl enable flaskapp
 sudo systemctl restart nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
-
-cd /home/ubuntu/NewsEmotionsExtraction
-sudo python3 -m venv venv
-
-export FLASK_APP='ApplicationCore.app'
-gunicorn -b 0.0.0.0:5000 ApplicationCore.app:app
