0b9c078ed4c35c2ddcb22dfee96a4075d78aeef9
Antoine Yi-Cheng Tian <antoine186@hotmail.com>
2023-05-12 06:17:59 +0200

    Search issue test 2

--- ApplicationCore/routes/search/emo_search_blueprint.py
+++ ApplicationCore/routes/search/emo_search_blueprint.py
@@ -25,13 +25,14 @@ def emo_search():
         search_end_date = attrgetter(*attributes)(search_end_date)
         search_start_date = attrgetter(*attributes)(search_start_date)
 
-        """
         google_news = GNews(language='en', country='US', start_date = search_start_date, end_date = search_end_date, max_results = 20)
         results = google_news.get_news(payload['searchInput'])
 
         news_classifier = NewsClassifier(nn, results, google_news, model_max_characters_allowed, keyword_extractor_nn, payload['searchInput'], \
                                         search_start_date, search_end_date)
         emo_breakdown_result_metadata = news_classifier.get_emo_percentage_breakdown_with_leading_results()
+
+        """
 
         if emo_breakdown_result_metadata != None:
             emo_breakdown_result_metadata_json_data = json.dumps(emo_breakdown_result_metadata, indent=4, cls=GenericJsonEncoder)
