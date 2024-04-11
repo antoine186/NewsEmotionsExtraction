fb7508e67ae01232a20d25109eed8d8978d75815
Antoine Yi-Cheng Tian <33133769+antoine186@users.noreply.github.com>
2024-04-11 15:14:57 +0100

    Update app_start_helper.py

--- ApplicationCore/app_start_helper.py
+++ ApplicationCore/app_start_helper.py
@@ -15,7 +15,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = "noreply@emomachines.xyz"
-app.config['MAIL_PASSWORD'] = "jdztxwrikhatwrcn"
+app.config['MAIL_PASSWORD'] = ""
 
 mail = Mail(app)
 
